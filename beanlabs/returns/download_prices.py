c73a2a952a696b6ff15458349241be97e1b68fee
Martin Blais <blais@furius.ca>
2020-12-28 01:27:25 -0500

    Moved function to beancount.

--- beanlabs/returns/download_prices.py
+++ beanlabs/returns/download_prices.py
@@ -17,40 +17,11 @@ import dateutil.parser
 
 from beancount import loader
 from beancount.core import data
+from beancount.core import number
 from beancount.core import amount
 from beancount.parser import printer
 
 from beanprice.sources import yahoo
-
-
-def infer_quantization(prices: List[Union[float, Decimal]],
-                       threshold=0.01) -> Optional[Decimal]:
-    """Given a list of price quotes, infer the quantization.
-
-    This simple algorithm increases the precision until all rounding errors are
-    below a fractional threshold.
-
-    Args:
-      prices: A list of float or Decimal prices to infer from. If floats are
-        provided, conversion is done naively.
-      threshold: A fraction, the maximum error to tolerate before stopping the
-        search.
-    Returns:
-      A decimal object to use with decimal.Decimal.quantize().
-    """
-    # Ensure all prices are decimal instances.
-    cprices = [price if isinstance(price, Decimal) else Decimal(price)
-               for price in prices]
-
-    # Search for the correct fraction to use.
-    for exponent in range(1, 20):
-        multiplier = Decimal(10**exponent)
-        quant = 1 / multiplier
-        residuals = [(price.quantize(quant) - price) * multiplier for price in prices]
-        max_residual = max(residuals)
-        if max_residual < threshold:
-            return quant
-    return None
 
 
 def main():
@@ -79,7 +50,7 @@ def main():
         raise RuntimeError("Could not fetch from {}".format(source))
 
     # Attempt to infer the right quantization and quantize if succesfull.
-    quant = infer_quantization([s.price for s in sprices])
+    quant = number.infer_quantization_from_numbers([s.price for s in sprices])
     if quant:
         sprices = [sprice._replace(price=sprice.price.quantize(quant))
                    for sprice in sprices]
