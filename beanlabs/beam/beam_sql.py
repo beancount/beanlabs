d2b8d5344e987a25edf28b066ba8c8ca4f14c19c
Martin Blais <blais@furius.ca>
2021-04-29 00:35:21 -0400

    Attempting to fix SQL with Beam.

--- beanlabs/beam/beam_sql.py
+++ beanlabs/beam/beam_sql.py
@@ -20,8 +20,17 @@ from apache_beam.transforms.sql import SqlTransform
 
 from apache_beam.options import pipeline_options
 from apache_beam.portability import python_urns
-from apache_beam.portability.api import beam_runner_api_pb2
-from apache_beam.runners.portability import fn_api_runner
+# from apache_beam.portability.api import beam_runner_api_pb2
+# from apache_beam.runners.portability import fn_api_runner
+
+import apache_beam as beam
+from apache_beam import coders
+from apache_beam.io import ReadFromText
+from apache_beam.io import WriteToText
+from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.options.pipeline_options import SetupOptions
+from apache_beam.runners.portability import portable_runner
+
 
 from beancount.core.data import Posting
 from beancount import loader
@@ -34,11 +43,19 @@ from beancount.core.position import Position
 from beanlabs.beam import beanbeam
 
 
+# coders.registry.register_coder(data.Posting, coders.RowCoder)
+# coders.registry.register_coder(data.Amount, coders.RowCoder)
+# coders.registry.register_coder(data.Cost, coders.RowCoder)
+# coders.registry.register_coder(data.CostSpec, coders.RowCoder)
+# coders.registry.register_coder(data.Meta, coders.RowCoder)
+
+
 def CreatePipeline(pipeline_args):
     poptions = pipeline_options.PipelineOptions(
         pipeline_args,
         runner="directrunner",
-        direct_running_mode="multi_threading")
+        direct_running_mode="multi_threading"
+    )
     # (options.viewas(BeamSqlPipelineOptions)
     #  .setPlannerName("org.apache.beam.sdk.extensions.sql.zetasql.ZetaSQLQueryPlanner"))
     return beam.Pipeline(options=poptions)
@@ -54,7 +71,7 @@ def main():
     logging.info("Reading ledger.")
     t1 = time.time()
     entries, errors, options_map = loader.load_file(args.filename)
-    postings = (posting
+    postings = (beam.Row(posting.account, posting.units.number, posting.units.currency)
                 for entry in data.filter_txns(entries)
                 for posting in entry.postings)
     price_map = prices.build_price_map(entries)
@@ -66,7 +83,7 @@ def main():
              | beam.Create(postings)
              | SqlTransform("""
                  SELECT account FROM PCOLLECTION
-             """, "zetasql")
+             """, dialect="zetasql")
              | beam.Map(print))
 
 
