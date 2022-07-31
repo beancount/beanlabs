b4d434178703308909ec5afd0b262ca2fc39d69e
blais <blais@furius.ca>
2022-07-31 11:28:48 -0400

    Added/modified scripts for listing active accounts.

--- beanlabs/scripts/list-active-accounts.py
+++ beanlabs/scripts/list-active-accounts.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
-"""List all open accounts.
-
-This is a script that can produce a comprehensive list of all open accounts.
-This is helpful when you want to revise your ledger to find accounts that should
-have been closed.
+"""List active balance sheet accounts, highlighting ones that might an update.
 """
 __copyright__ = "Copyright (C) 2022  Martin Blais"
 __license__ = "GNU GPLv2"
@@ -12,6 +8,7 @@ import argparse
 import re
 import pprint
 import datetime as dt
+from functools import partial
 
 import petl
 
@@ -46,7 +43,8 @@ def main():
 
     ocmap = getters.get_account_open_close(entries)
     accounts = [
-        (account, open_.date) for account, (open_, close) in ocmap.items() if not close
+        (account, open_.date, close.date if close else None)
+        for account, (open_, close) in ocmap.items()
     ]
 
     def get_last_date(account):
@@ -64,37 +62,50 @@ def main():
 
     def get_sort_key(rec):
         account_type = account_types.get_account_type(rec.account)
-        if account_type in (acctypes.assets,
-                            acctypes.liabilities,
-                            acctypes.income):
+        if account_type in (acctypes.assets, acctypes.liabilities, acctypes.income):
             return (0, rec.account_sans, rec.type)
         else:
             return (1, rec.type, rec.account_sans)
 
     one_day = dt.timedelta(days=1)
 
-    def get_close_directive(rec):
-        close_date = rec.last_date + one_day
-        return f"{close_date} close {rec.account}"
+    def is_leaf_or_active(rec):
+        real_account = realization.get(real, rec.account)
+        return len(real_account) == 0 or real_account.txn_postings
 
-    table = petl.wrap([("account", "open_date")] + accounts)
+    table = petl.wrap([("account", "open_date", "close_date")] + accounts)
     today = dt.date.today()
     table = (
-        table.addfield("type", lambda r: account_types.get_account_type(r.account))
+        table
+        # Consider only balance sheet accounts.
+        .select(lambda r: account_types.is_balance_sheet_account(r.account, acctypes))
+        # Remove closed accounts.
+        .selecteq("close_date", None)
+        .cutout("close_date")
+        # Remove accounts with no postings and some children.
+        .select(is_leaf_or_active)
+        # Break out account components
+        .addfield("type", lambda r: account_types.get_account_type(r.account))
         .addfield("account_sans", lambda r: account.sans_root(r.account))
-        .addfield("sortkey", get_sort_key)
-        .addfield("postings", lambda r: sum(1 if isinstance(tp, TxnPosting) else 0
-                                            for tp in realization.get(real, r.account).txn_postings))
+        # Add data about the number of postings.
+        .addfield(
+            "postings",
+            lambda r: sum(
+                1 if isinstance(tp, TxnPosting) else 0
+                for tp in realization.get(real, r.account).txn_postings
+            ),
+        )
         .addfield("last_date", lambda r: get_last_date(r.account))
         .addfield("days_open", lambda r: (today - r.open_date).days)
         .addfield("days_active", lambda r: (r.last_date - r.open_date).days)
         .addfield("days_since", lambda r: (today - r.last_date).days)
         .addfield("balance", lambda r: get_balance(r.account))
-        .addfield("close", get_close_directive)
-        .cutout("account")
-        .movefield("close", 0)
         .movefield("type", 1)
         .movefield("account_sans", 2)
+        # Custom sort that will keep the portion below the account type
+        # together.
+        .addfield("sortkey", get_sort_key)
+        .cutout("account")
         .sort("sortkey")
         .cutout("sortkey")
     )
